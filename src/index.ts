47426de84e148975013eab805d153760f35c19fb
Kumar <Naveen.Kumar@acer.com>
2018-10-12 14:28:19 +0400

    Initial changes

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ const resolvers: ResolverMap = {
         .getMany();
       console.log("====================================");
       console.log(uProfile);
-      console.log("====================================");
+      console.log("========================================");
     }
   },
   Mutation: {
